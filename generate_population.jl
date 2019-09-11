cf3eef7c4dcb48d3e7c90acf0f5711f3e4e52127
steamworks <adell892@live.kutztown.edu>
2019-09-11 11:04:32 -0400

    generated more initial populations

--- generate_population.jl
+++ generate_population.jl
@@ -21,7 +21,7 @@ const problems_dir = "beasley_mdmkp_datasets/"
 const results_dir = problems_dir
 
 function main(;verbose::Int=0)
-	for dataset in [9]
+	for dataset in 1:9
 	    problems = parse_file(problems_dir * "mdmkp_ct$(dataset).txt")
 
 		results = Vector{Vector{BitList}}()
