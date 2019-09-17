26c2a2dfd009439803c45414a9b65d03fcc09739
steamworks <adell892@live.kutztown.edu>
2019-09-17 14:45:08 -0400

    made graph

--- improvement_points_graph.py
+++ improvement_points_graph.py
@@ -6,7 +6,46 @@ with open("test_data.json", "r") as file:
     data = file.read()
     data = json.loads(data)
 
-for alg in data:
+
+def repair_holes(ip):
+    i = 1
+    repaired = []
+    repaired.append(ip[0])
+    prevtries, prevtotal, prevbest = ip[0]
+    while i < len(ip):
+        tries, total, best = ip[i]
+        j = 1
+        while not (tries == repaired[-1][0] + 1):
+            repaired.append([prevtries + j, prevtotal, prevbest])
+            j += 1
+        repaired.append([tries, total, best])
+        prevtries, prevtotal, prevbest = tries, total, best
+        i += 1
+    return repaired
+
+def split(ip, n_fails=25):
+    split_index = 0
+    fails = 0
+    prev_score = 0
+    for point in ip:
+        tries, total, best = point
+        if total == prev_score:
+            fails += 1
+        else:
+            prev_score = total
+            fails = 0
+        if fails >= n_fails:
+            split_index = tries - 1
+            break
+    split_index = max(split_index, 1)
+    first_half = ip[:split_index]
+    second_half = ip[split_index:]
+    print(split_index)
+    return first_half, second_half
+
+
+algs = ["jaya[VND]"]
+for alg in algs:
     plt.title(alg)
 
     lines = []
@@ -14,8 +53,13 @@ for alg in data:
         lines.append(problem_results["improvement_points"])
 
     for line in lines:
-        x = [l[0] for l in line]
-        y = MinMaxScaler().fit_transform([[l[1]] for l in line])
-        plt.plot(x, y)
+        first, second = split(repair_holes(line))
+        adjust = first[0][1]
+        xf = [l[0] for l in first]
+        yf = [l[1] - adjust for l in first]
+        xs = [l[0] for l in second]
+        ys = [l[1] - adjust for l in second]
+        plt.plot(xf, yf, "k", linewidth=.3)
+        plt.plot(xs, ys, "r", linewidth=.3)
 
     plt.show()
