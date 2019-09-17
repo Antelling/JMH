cc57b5e37d4c86cb80cfe6220dd5c6f0d6bd4866
Anthony Dellinger <adell892@live.kutztown.edu>
2019-09-17 18:47:14 -0400

    worked on graphs

--- improvement_points_graph.py
+++ improvement_points_graph.py
@@ -1,8 +1,9 @@
 import json
 from matplotlib import pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
+import numpy as np
 
-with open("test_data.json", "r") as file:
+with open("results/CGA2_ls_10s_5000f_pop30_ls/3.json", "r") as file:
     data = file.read()
     data = json.loads(data)
 
@@ -37,29 +38,41 @@ def split(ip, n_fails=25):
         if fails >= n_fails:
             split_index = tries - 1
             break
-    split_index = max(split_index, 1)
+    else:
+        split_index = ip[-1][0] - 1
     first_half = ip[:split_index]
     second_half = ip[split_index:]
-    print(split_index)
     return first_half, second_half
 
 
-algs = ["jaya[VND]"]
-for alg in algs:
+def get_score(ip, n_fails):
+    first, second = split(ip, n_fails)
+    return first[-1][2]/ip[-1][2]
+
+for alg in data:
     plt.title(alg)
 
     lines = []
     for problem_results in data[alg]:
         lines.append(problem_results["improvement_points"])
 
+
+
+    all_scores = []
     for line in lines:
-        first, second = split(repair_holes(line))
-        adjust = first[0][1]
-        xf = [l[0] for l in first]
-        yf = [l[1] - adjust for l in first]
-        xs = [l[0] for l in second]
-        ys = [l[1] - adjust for l in second]
-        plt.plot(xf, yf, "k", linewidth=.3)
-        plt.plot(xs, ys, "r", linewidth=.3)
+        line = repair_holes(line)
+        scores = [[get_score(line, n)] for n in range(1, 200)]
+        all_scores.append(scores)
+    all_scores = np.mean(all_scores, axis=0)
+    plt.plot(all_scores)
+
+    """first, second = split(repair_holes(line), 5)
+    adjust = first[0][2]
+    xf = [l[0] for l in first]
+    yf = [l[2] - adjust for l in first]
+    xs = [l[0] - 1 for l in second]
+    ys = [l[2] - adjust for l in second]
+    plt.plot(xf, yf, "k", linewidth=.3)
+    plt.plot(xs, ys, "r", linewidth=.3)"""
 
     plt.show()
--- main.jl
+++ main.jl
@@ -18,10 +18,10 @@ using Dates: today
 # import Random
 
 const problems_dir = "beasley_mdmkp_datasets/"
-const n_fails = 50
+const n_fails = 5000
 const time_limit = 10
 const initial_pop_dir_suffix = "pop30_ls"
-const results_dir = "results/rao_compare_$(time_limit)s_$(n_fails)f_$initial_pop_dir_suffix/"
+const results_dir = "results/CGA2_ls_$(time_limit)s_$(n_fails)f_$initial_pop_dir_suffix/"
 run(`mkdir -p $(results_dir)`)
 
 struct ResultSet
@@ -36,23 +36,10 @@ function main(;verbose::Int=0)
 
 	algorithms = [
 		(control_monad(), "control"),
-		(VND_monad(), "VND"),
-		(LF_monad(), "LF"),
-		(iterate_monad(jaya_monad(perturb=jaya_perturb), time_limit=time_limit, n_fails=n_fails), "jaya"),
-		(iterate_monad(jaya_monad(perturb=rao1_perturb), time_limit=time_limit, n_fails=n_fails), "rao1"),
-		(iterate_monad(jaya_monad(perturb=jaya_perturb, local_search=VND), time_limit=time_limit, n_fails=n_fails), "jaya[VND]"),
-		(iterate_monad(jaya_monad(perturb=rao1_perturb, local_search=VND), time_limit=time_limit, n_fails=n_fails), "rao1[VND]"),
-		(iterate_monad(jaya_monad(perturb=jaya_perturb, local_search=local_flip), time_limit=time_limit, n_fails=n_fails), "jaya[LF]"),
-		(iterate_monad(jaya_monad(perturb=rao1_perturb, local_search=local_flip), time_limit=time_limit, n_fails=n_fails), "rao1[LF]"),
-		(skate_monad([jaya_monad(top_n=1, bottom_n=1, perturb=jaya_perturb), VND_monad()], time_limit=time_limit, n_fails=n_fails), "jaya&VND"),
-		(skate_monad([jaya_monad(top_n=1, bottom_n=1, perturb=rao1_perturb), VND_monad()], time_limit=time_limit, n_fails=n_fails), "rao1&VND"),
-		(skate_monad([jaya_monad(top_n=1, bottom_n=1, perturb=jaya_perturb), LF_monad()], time_limit=time_limit, n_fails=n_fails), "jaya&LF"),
-		(skate_monad([jaya_monad(top_n=1, bottom_n=1, perturb=rao1_perturb), LF_monad()], time_limit=time_limit, n_fails=n_fails), "rao1&LF"),
-		(ordered_applicator_monad([iterate_monad(jaya_monad(perturb=jaya_perturb), time_limit=time_limit, n_fails=n_fails), VND_monad()]), "jaya-->VND"),
-		(ordered_applicator_monad([iterate_monad(jaya_monad(perturb=rao1_perturb), time_limit=time_limit, n_fails=n_fails), VND_monad()]), "rao1-->VND")
+		(iterate_monad(PGA_monad(local_search=VND, n_parents=2), n_fails=n_fails, time_limit=time_limit), "PGA2_ls")
 	]
 
-	for dataset in 1:4
+	for dataset in [6]
 	    problems = parse_file(problems_dir * "mdmkp_ct$(dataset).txt")
 		populations::Vector{Vector{BitList}} = JSON.parsefile(problems_dir * "$(dataset)_$(initial_pop_dir_suffix).json")
 
@@ -110,10 +97,10 @@ function main(;verbose::Int=0)
 			if verbose > 0
 				@assert p == "$(problem)"
 			end
-		end
 
-		open(results_dir * "$(dataset).json", "w") do f
-			write(f, JSON.json(results, 4))
+			open(results_dir * "$(dataset).json", "w") do f
+				write(f, JSON.json(results, 4))
+			end
 		end
 	end
 end
@@ -150,108 +137,18 @@ iterate_monad(
 capstone = (ordered_applicator_monad(input), "capstone")
 
 
-algorithms = [(control_monad(), "control"),
-	(LF_monad(), "LF"),
-	(LS_monad(), "LS"),
-	(VND_monad(), "VND"),
-
-	(iterate_monad(TBO_monad(local_search=VND, top_n=1), n_fails=n_fails, time_limit=time_limit), "T1_ls"),
-	(iterate_monad(TBO_monad(local_search=VND, top_n=3), n_fails=n_fails, time_limit=time_limit), "T3_ls"),
-	(iterate_monad(TBO_monad(local_search=VND, top_n=7), n_fails=n_fails, time_limit=time_limit), "T7_ls"),
-	(iterate_monad(TBO_monad(local_search=VND, top_n=15), n_fails=n_fails, time_limit=time_limit), "T15_ls"),
-	(iterate_monad(TBO_monad(local_search=VND, top_n=30), n_fails=n_fails, time_limit=time_limit), "T30_ls"),
-
-	(iterate_monad(TBO_monad(local_search=identity, top_n=1), n_fails=n_fails, time_limit=time_limit), "T1"),
-	(iterate_monad(TBO_monad(local_search=identity, top_n=3), n_fails=n_fails, time_limit=time_limit), "T3"),
-	(iterate_monad(TBO_monad(local_search=identity, top_n=7), n_fails=n_fails, time_limit=time_limit), "T7"),
-	(iterate_monad(TBO_monad(local_search=identity, top_n=15), n_fails=n_fails, time_limit=time_limit), "T15"),
-	(iterate_monad(TBO_monad(local_search=identity, top_n=30), n_fails=n_fails, time_limit=time_limit), "T30"),
-
-	(iterate_monad(TBO_monad(local_search=VND, top_n=1, v2=true), n_fails=n_fails, time_limit=time_limit), "T1_ls_v2"),
-	(iterate_monad(TBO_monad(local_search=VND, top_n=3, v2=true), n_fails=n_fails, time_limit=time_limit), "T3_ls_v2"),
-	(iterate_monad(TBO_monad(local_search=VND, top_n=7, v2=true), n_fails=n_fails, time_limit=time_limit), "T7_ls_v2"),
-	(iterate_monad(TBO_monad(local_search=VND, top_n=15, v2=true), n_fails=n_fails, time_limit=time_limit), "T15_ls_v2"),
-	(iterate_monad(TBO_monad(local_search=VND, top_n=30, v2=true), n_fails=n_fails, time_limit=time_limit), "T30_ls_v2"),
-
-	(iterate_monad(TBO_monad(local_search=identity, top_n=1, v2=true), n_fails=n_fails, time_limit=time_limit), "T1_v2"),
-	(iterate_monad(TBO_monad(local_search=identity, top_n=3, v2=true), n_fails=n_fails, time_limit=time_limit), "T3_v2"),
-	(iterate_monad(TBO_monad(local_search=identity, top_n=7, v2=true), n_fails=n_fails, time_limit=time_limit), "T7_v2"),
-	(iterate_monad(TBO_monad(local_search=identity, top_n=15, v2=true), n_fails=n_fails, time_limit=time_limit), "T15_v2"),
-	(iterate_monad(TBO_monad(local_search=identity, top_n=30, v2=true), n_fails=n_fails, time_limit=time_limit), "T30_v2"),
-
-
-	(iterate_monad(CBO_monad(local_search=VND, bottom_n=1), n_fails=n_fails, time_limit=time_limit), "C1_ls"),
-	(iterate_monad(CBO_monad(local_search=VND, bottom_n=3), n_fails=n_fails, time_limit=time_limit), "C3_ls"),
-	(iterate_monad(CBO_monad(local_search=VND, bottom_n=7), n_fails=n_fails, time_limit=time_limit), "C7_ls"),
-	(iterate_monad(CBO_monad(local_search=VND, bottom_n=15), n_fails=n_fails, time_limit=time_limit), "C15_ls"),
-	(iterate_monad(CBO_monad(local_search=VND, bottom_n=30), n_fails=n_fails, time_limit=time_limit), "C30_ls"),
-
-	(iterate_monad(CBO_monad(local_search=identity, bottom_n=1), n_fails=n_fails, time_limit=time_limit), "C1"),
-	(iterate_monad(CBO_monad(local_search=identity, bottom_n=3), n_fails=n_fails, time_limit=time_limit), "C3"),
-	(iterate_monad(CBO_monad(local_search=identity, bottom_n=7), n_fails=n_fails, time_limit=time_limit), "C7"),
-	(iterate_monad(CBO_monad(local_search=identity, bottom_n=15), n_fails=n_fails, time_limit=time_limit), "C15"),
-	(iterate_monad(CBO_monad(local_search=identity, bottom_n=30), n_fails=n_fails, time_limit=time_limit), "C30"),
-
-	(iterate_monad(CBO_monad(local_search=VND, bottom_n=1, v2=true), n_fails=n_fails, time_limit=time_limit), "C1_ls_v2"),
-	(iterate_monad(CBO_monad(local_search=VND, bottom_n=3, v2=true), n_fails=n_fails, time_limit=time_limit), "C3_ls_v2"),
-	(iterate_monad(CBO_monad(local_search=VND, bottom_n=7, v2=true), n_fails=n_fails, time_limit=time_limit), "C7_ls_v2"),
-	(iterate_monad(CBO_monad(local_search=VND, bottom_n=15, v2=true), n_fails=n_fails, time_limit=time_limit), "C15_ls_v2"),
-	(iterate_monad(CBO_monad(local_search=VND, bottom_n=30, v2=true), n_fails=n_fails, time_limit=time_limit), "C30_ls_v2"),
-
-	(iterate_monad(CBO_monad(local_search=identity, bottom_n=1, v2=true), n_fails=n_fails, time_limit=time_limit), "C1_v2"),
-	(iterate_monad(CBO_monad(local_search=identity, bottom_n=3, v2=true), n_fails=n_fails, time_limit=time_limit), "C3_v2"),
-	(iterate_monad(CBO_monad(local_search=identity, bottom_n=7, v2=true), n_fails=n_fails, time_limit=time_limit), "C7_v2"),
-	(iterate_monad(CBO_monad(local_search=identity, bottom_n=15, v2=true), n_fails=n_fails, time_limit=time_limit), "C15_v2"),
-	(iterate_monad(CBO_monad(local_search=identity, bottom_n=30, v2=true), n_fails=n_fails, time_limit=time_limit), "C30_v2"),
-
-
-	(iterate_monad(LBO_monad(local_search=VND)), "LBO_ls"),
-	(iterate_monad(LBO_monad(local_search=identity)), "LBO"),
-	(iterate_monad(LBO_monad(local_search=VND, v2=true)), "LBO_ls_v2"),
-	(iterate_monad(LBO_monad(local_search=identity, v2=true)), "LBO_v2"),
-
-
-	(iterate_monad(TLBO_monad(local_search=identity)), "TLBO"),
-	(iterate_monad(TLBO_monad(local_search=VND)), "TLBO_ls"),
-	(iterate_monad(TLBO_monad(local_search=identity, v2=true)), "TLBO_v2"),
-	(iterate_monad(TLBO_monad(local_search=VND, v2=true)), "TLBO_ls_v2"),
-
-
-	(iterate_monad(GA_monad(n_parents=2, local_search=identity)), "GA2"),
-	(iterate_monad(GA_monad(n_parents=5, local_search=identity)), "GA5"),
-	(iterate_monad(GA_monad(n_parents=10, local_search=identity)), "GA10"),
-	(iterate_monad(GA_monad(n_parents=20, local_search=identity)), "GA20"),
-	(iterate_monad(GA_monad(n_parents=30, local_search=identity)), "GA30"),
-
-	(iterate_monad(GA_monad(n_parents=2, local_search=VND)), "GA2_ls"),
-	(iterate_monad(GA_monad(n_parents=5, local_search=VND)), "GA5_ls"),
-	(iterate_monad(GA_monad(n_parents=10, local_search=VND)), "GA10_ls"),
-	(iterate_monad(GA_monad(n_parents=20, local_search=VND)), "GA20_ls"),
-	(iterate_monad(GA_monad(n_parents=30, local_search=VND)), "GA30_ls"),
-
-
-	(iterate_monad(sloppy_jaya_monad(top_n=1, bottom_n=1)), "J1"),
-	(iterate_monad(sloppy_jaya_monad(top_n=3, bottom_n=3)), "J3"),
-	(iterate_monad(sloppy_jaya_monad(top_n=7, bottom_n=7)), "J7"),
-	(iterate_monad(sloppy_jaya_monad(top_n=15, bottom_n=15)), "J15"),
-	(iterate_monad(sloppy_jaya_monad(top_n=29, bottom_n=29)), "J30"),
-
-	(iterate_monad(sloppy_jaya_monad(local_search=VND, top_n=1, bottom_n=1)), "J1_ls"),
-	(iterate_monad(sloppy_jaya_monad(local_search=VND, top_n=3, bottom_n=3)), "J3_ls"),
-	(iterate_monad(sloppy_jaya_monad(local_search=VND, top_n=7, bottom_n=7)), "J7_ls"),
-	(iterate_monad(sloppy_jaya_monad(local_search=VND, top_n=15, bottom_n=15)), "J15_ls"),
-	(iterate_monad(sloppy_jaya_monad(local_search=VND, top_n=29, bottom_n=29)), "J30_ls"),
-	#
-	(iterate_monad(sloppy_jaya_monad(top_n=1, bottom_n=1, v2=true)), "J1_v2"),
-	(iterate_monad(sloppy_jaya_monad(top_n=3, bottom_n=3, v2=true)), "J3_v2"),
-	(iterate_monad(sloppy_jaya_monad(top_n=7, bottom_n=7, v2=true)), "J7_v2"),
-	(iterate_monad(sloppy_jaya_monad(top_n=15, bottom_n=15, v2=true)), "J15_v2"),
-	(iterate_monad(sloppy_jaya_monad(top_n=29, bottom_n=29, v2=true)), "J30_v2"),
-
-	(iterate_monad(sloppy_jaya_monad(local_search=VND, top_n=1, bottom_n=1, v2=true)), "J1_ls_v2"),
-	(iterate_monad(sloppy_jaya_monad(local_search=VND, top_n=3, bottom_n=3, v2=true)), "J3_ls_v2"),
-	(iterate_monad(sloppy_jaya_monad(local_search=VND, top_n=7, bottom_n=7, v2=true)), "J7_ls_v2"),
-	(iterate_monad(sloppy_jaya_monad(local_search=VND, top_n=15, bottom_n=15, v2=true)), "J15_ls_v2"),
-	(iterate_monad(sloppy_jaya_monad(local_search=VND, top_n=29, bottom_n=29, v2=true)), "J30_ls_v2"),
-]
+(VND_monad(), "VND"),
+(LF_monad(), "LF"),
+(iterate_monad(jaya_monad(perturb=jaya_perturb), time_limit=time_limit, n_fails=n_fails), "jaya"),
+(iterate_monad(jaya_monad(perturb=rao1_perturb), time_limit=time_limit, n_fails=n_fails), "rao1"),
+(iterate_monad(jaya_monad(perturb=jaya_perturb, local_search=VND), time_limit=time_limit, n_fails=n_fails), "jaya[VND]"),
+(iterate_monad(jaya_monad(perturb=rao1_perturb, local_search=VND), time_limit=time_limit, n_fails=n_fails), "rao1[VND]"),
+(iterate_monad(jaya_monad(perturb=jaya_perturb, local_search=local_flip), time_limit=time_limit, n_fails=n_fails), "jaya[LF]"),
+(iterate_monad(jaya_monad(perturb=rao1_perturb, local_search=local_flip), time_limit=time_limit, n_fails=n_fails), "rao1[LF]"),
+(skate_monad([jaya_monad(top_n=1, bottom_n=1, perturb=jaya_perturb), VND_monad()], time_limit=time_limit, n_fails=n_fails), "jaya&VND"),
+(skate_monad([jaya_monad(top_n=1, bottom_n=1, perturb=rao1_perturb), VND_monad()], time_limit=time_limit, n_fails=n_fails), "rao1&VND"),
+(skate_monad([jaya_monad(top_n=1, bottom_n=1, perturb=jaya_perturb), LF_monad()], time_limit=time_limit, n_fails=n_fails), "jaya&LF"),
+(skate_monad([jaya_monad(top_n=1, bottom_n=1, perturb=rao1_perturb), LF_monad()], time_limit=time_limit, n_fails=n_fails), "rao1&LF"),
+(ordered_applicator_monad([iterate_monad(jaya_monad(perturb=jaya_perturb), time_limit=time_limit, n_fails=n_fails), VND_monad()]), "jaya-->VND"),
+(ordered_applicator_monad([iterate_monad(jaya_monad(perturb=rao1_perturb), time_limit=time_limit, n_fails=n_fails), VND_monad()]), "rao1-->VND")
 """
